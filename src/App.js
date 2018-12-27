c47df8323cf088a65e88104e3a9b8a82bbd5fc8e
Andy Ly <andywindsong@gmail.com>
2018-12-26 20:51:52 -0800

    react router added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <div className="demo-big-content">
       {/* Uses a header that scrolls with the text, rather than staying locked at the top */}
-        <div className="demo-big-content">
-            <Layout>
-                <Header title="Title" scroll>
-                    <Navigation>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                    </Navigation>
-                </Header>
-                <Drawer title="Title">
-                    <Navigation>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                        <a href="#">Link</a>
-                    </Navigation>
-                </Drawer>
-                <Content>
-                    <div className="page-content" />
-                </Content>
-            </Layout>
-        </div>
+          <Layout>
+              <Header title="Title" scroll>
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Header>
+              <Drawer title="Title">
+                  <Navigation>
+                      <Link to="/resume">Resume</Link>
+                      <Link to="/aboutme">About Me</Link>
+                      <Link to="/projects">Projects</Link>
+                      <Link to="/contact">Contact</Link>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+                  <Main />
+              </Content>
+          </Layout>
+      </div>
     );
   }
 }
