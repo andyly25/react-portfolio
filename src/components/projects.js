ad0ea8105b34329f33c55e688fc0054bc4facab4
Andy Ly <andywindsong@gmail.com>
2018-12-31 16:41:26 -0800

    added in tabs

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell } from 'react-mdl';
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+        switch(this.state.activeTab) {
+            case 0:
+                return(
+                    <div><h1>This is React</h1></div>
+                 );
+            case 1:
+                return(
+                    <div><h1>This is MongoDB</h1></div>
+                 );
+            case 2:
+                return(
+                    <div><h1>This is Python</h1></div>
+                 );
+            case 3:
+                return(
+                    <div><h1>This is Etc...</h1></div>
+                 );
+            default:
+                return(
+                    <div><h1>How did you get here?!</h1></div>
+                 );
+        }
+        // if (this.state.activeTab === 0) {
+        //     return(
+        //         <div><h1>This is React</h1></div>
+        //     );
+        // } else if (this.state.activeTab === 1) {
+        //     <div><h1></h1></div>
+        // }
+    }
+
     render() {
         return(
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>MongoDB</Tab>
+                    <Tab>Python</Tab>
+                    <Tab>Etc...</Tab>
+                </Tabs>
+                <section className="projects-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                            <div className="content">{this.toggleCategories()}</div>
+                        </Cell>
+                    </Grid>
+                </section>
             </div>
         );
     }
