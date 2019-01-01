71ac3f4dd1d96f00ab7994539f47fa1dc259d717
Andy Ly <andywindsong@gmail.com>
2018-12-31 17:21:56 -0800

    sample projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell } from 'react-mdl';
+import {
+    Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton }
+    from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -8,10 +10,58 @@ class Projects extends Component {
     }
 
     toggleCategories() {
+        // NOTE! this is very bad copy and paste and definitely needs to be refactored later after
+        // everything works perfectly!
         switch(this.state.activeTab) {
             case 0:
                 return(
-                    <div><h1>This is React</h1></div>
+                    <div className="projects-grid">
+                        {/*Project 1*/}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}} >
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+                            <CardText>
+                                lorem ipsum spam lorem ipsum spam lorem ipsum spam lorem ipsum spam lorem ipsum spam
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        {/*Project 2*/}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}} >
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+                            <CardText>
+                                lorem ipsum spam lorem ipsum spam lorem ipsum spam lorem ipsum spam lorem ipsum spam
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                        {/*Project 3*/}
+                        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}} >
+                            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://cdn.worldvectorlogo.com/logos/react-1.svg) center / cover'}} >React Project #3</CardTitle>
+                            <CardText>
+                                lorem ipsum spam lorem ipsum spam lorem ipsum spam lorem ipsum spam lorem ipsum spam
+                            </CardText>
+                            <CardActions border>
+                                <Button colored>GitHub</Button>
+                                <Button colored>CodePen</Button>
+                                <Button colored>LiveDemo</Button>
+                            </CardActions>
+                            <CardMenu style={{color: '#fff'}}>
+                                <IconButton name="share" />
+                            </CardMenu>
+                        </Card>
+                    </div>
                  );
             case 1:
                 return(
@@ -48,13 +98,11 @@ class Projects extends Component {
                     <Tab>Python</Tab>
                     <Tab>Etc...</Tab>
                 </Tabs>
-                <section className="projects-grid">
-                    <Grid className="projects-grid">
-                        <Cell col={12}>
-                            <div className="content">{this.toggleCategories()}</div>
-                        </Cell>
-                    </Grid>
-                </section>
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">{this.toggleCategories()}</div>
+                    </Cell>
+                </Grid>
             </div>
         );
     }
